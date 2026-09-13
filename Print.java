c802b768dbf3b615458ecadfb947fba187664f39
halim <abdhalym522@gmail.com>
2026-09-14 06:57:25 +0800

    update Print.java

---
+++ Print.java
@@ -0,0 +1,22 @@
+
+
+public class EvaluasiNilai {
+    public static void main(String[] args) {
+
+ 
+   String nama = "Ariel";
+int umur = 20;
+double ipk = 3.75;
+char kelas = 'A';
+boolean lulus = true;
+System.out.printf("Nama: %s%n", nama);
+System.out.printf("Umur: %d%n", umur);
+System.out.printf("IPK: %f%n", ipk);
+System.out.printf("Kelas: %c%n", kelas);
+System.out.printf("Lulus: %b", lulus);
+
+
+
+      
+    }
+}
