40aa5ee7e16dd0efea8f590976972ef85b6490b8
halim <abdhalym522@gmail.com>
2026-09-14 07:06:47 +0800

    update Tabel.java

---
+++ Tabel.java
@@ -0,0 +1,16 @@
+
+
+public class EvaluasiNilai {
+    public static void main(String[] args) {
+
+ 
+   System.out.printf("%-10s %5s%n", "Nama", "Nilai");
+ System.out.printf("%-10s %5d%n", "Ariel", 90);
+ System.out.printf("%-10s %5d%n", "Budi", 85);
+ System.out.printf("%-10s %5d%n", "Citra", 95);
+
+
+
+      
+    }
+}
